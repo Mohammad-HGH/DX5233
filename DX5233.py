c188f953c584d495f23b4d39cc9d73567b4dc1b1
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-12-02 15:56:39 +0330

    first commit

---
+++ DX5233.py
@@ -0,0 +1,22 @@
+from lxml.etree import tostring
+import lxml.html
+import requests
+from adremover import AdRemover
+
+url = 'https://soft98.ir'  # replace it with a url you want to apply the rules to  
+rule_urls = ['ruadlist+easylist.txt', '1.txt']
+
+rule_files = [url.rpartition('/')[-1] for url in rule_urls]
+
+
+
+
+
+remover = AdRemover(*rule_files)
+
+html = requests.get(url).text
+document = lxml.html.document_fromstring(html)
+remover.remove_ads(document)
+clean_html = tostring(document).decode("utf-8")
+
+print(clean_html)
