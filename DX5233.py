94273539120842cc65d4e391d137851b6e3c2e2d
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2022-12-02 22:52:31 +0330

    decode utf8 to str doc to store in html file

--- DX5233.py
+++ DX5233.py
@@ -4,7 +4,7 @@ import requests
 from adremover import AdRemover
 from bs4 import BeautifulSoup
 
-url = 'https://soft98.ir'  # replace it with a url you want to apply the rules to  
+url = 'https://www.zoomit.ir'  # replace it with a url you want to apply the rules to  
 rule_urls = ['ruadlist+easylist.txt', '1.txt']
 
 rule_files = [url.rpartition('/')[-1] for url in rule_urls]
@@ -19,7 +19,7 @@ html = requests.get(url).text
 document = lxml.html.document_fromstring(html)
 remover.remove_ads(document)
 clean_html = tostring(document).decode("utf-8")
-
+soup=BeautifulSoup(clean_html)
 # print(BeautifulSoup(clean_html))
-with open('readme.html', 'w') as f:
-    f.write(clean_html)
+with open('readme.html', 'w',encoding='utf-8') as f:
+    f.write(str(soup))
