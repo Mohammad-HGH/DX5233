c9b2895b2d85f924128b918112b6e17afff8fa3f
mohammad-hgh <88213502+Mohammad-HGH@users.noreply.github.com>
2023-01-03 19:46:47 +0330

    with GUI window

--- index.py
+++ index.py
@@ -1,60 +1,45 @@
-# # Import Module
-# from bs4 import BeautifulSoup
-# import requests
-
-# # Website URL
-# URL = 'https://soft98.ir/'
-
-# # Page content from Website URL
-# page = requests.get(URL).text
-
-
-# soup = BeautifulSoup(page, 'html.parser')
-# for s in soup.select('script'):
-#     s.extract()
-# # print(soup)
-# for s in soup.select('img'):
-#     s.extract()
-# with open('readme.html', 'w',encoding='utf-8') as f:
-#     f.write(str(soup))
+# Import Module
+from bs4 import BeautifulSoup
+import requests
 from tkinter import *
 
 
 class MyWindow:
     def __init__(self, win):
-        self.lbl1 = Label(win, text='First number')
-        self.lbl2 = Label(win, text='Second number')
-        self.lbl3 = Label(win, text='Result')
+        # set labels
+        self.lbl1 = Label(win, text='Address link')
+
+        # set input forms
         self.t1 = Entry(bd=3)
-        self.t2 = Entry()
-        self.t3 = Entry()
-        self.btn1 = Button(win, text='Add')
-        self.btn2 = Button(win, text='Subtract')
+
+        # set buttons
+        self.btn1 = Button(win, text='Remove Ads')
+
         self.lbl1.place(x=100, y=50)
         self.t1.place(x=200, y=50)
-        self.lbl2.place(x=100, y=100)
-        self.t2.place(x=200, y=100)
-        self.b1 = Button(win, text='Add', command=self.add)
-        self.b2 = Button(win, text='Subtract')
-        self.b2.bind('<Button-1>', self.sub)
+
+        self.b1 = Button(win, text='Add', command=self.adsRemover)
         self.b1.place(x=100, y=150)
-        self.b2.place(x=200, y=150)
-        self.lbl3.place(x=100, y=200)
-        self.t3.place(x=200, y=200)
 
-    def add(self):
-        self.t3.delete(0, 'end')
-        num1 = int(self.t1.get())
-        num2 = int(self.t2.get())
-        result = num1+num2
-        self.t3.insert(END, str(result))
+    def adsRemover(self):
+        # Website URL
+        URL = self.t1.get()
 
-    def sub(self, event):
-        self.t3.delete(0, 'end')
-        num1 = int(self.t1.get())
-        num2 = int(self.t2.get())
-        result = num1-num2
-        self.t3.insert(END, str(result))
+        # Page content from Website URL
+        page = requests.get(URL).text
+
+        soup = BeautifulSoup(page, 'html.parser')
+        for s in soup.select('script'):
+            s.extract()
+
+        for s in soup.select('img'):
+            s.extract()
+
+        for s in soup.select('video'):
+            s.extract()
+
+        with open('readme.html', 'w', encoding='utf-8') as f:
+            f.write(str(soup))
 
 
 window = Tk()
